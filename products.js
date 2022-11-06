607e6b2dcc19399b4f3e5ea57310d333a720af92
harsh saxena <harshsaxenajii@gmail.com>
2022-11-06 16:07:56 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 1,
+    name: "APPLE Iphone 11(White, 64GB)",
+    desc: "64GB RAM",
+    price: 40990,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543361/Products/Iphone-11_white_eu3wjr.png"
+  },
+  {
+    id: 2,
+    name: "APPLE Iphone 11(Green, 64GB)",
+    desc: "64GB RAM",
+    price: 40990,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543359/Products/Iphone-11_green_hr5bly.png"
+  },
+  {
+    id: 3,
+    name: "APPLE Iphone 11(Black, 64GB)",
+    desc: "64GB RAM",
+    price: 40990,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543360/Products/Iphone-11_black_qohlfm.png"
+  },
+  {
+    id: 4,
+    name: "APPLE Iphone 13(White, 128GB)",
+    desc: "128GB RAM",
+    price: 65999,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543363/Products/Iphone-13_white_avzsht.png"
+  },
+  {
+    id: 5,
+    name: "APPLE Iphone 13(Green, 128GB)",
+    desc: "128GB RAM",
+    price: 65999,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543362/Products/Iphone-13_green_n2lifq.png"
+  },
+  {
+    id: 6,
+    name: "APPLE Iphone 13(Black, 128GB)",
+    desc: "128GB RAM",
+    price: 65999,
+    Image:"https://res.cloudinary.com/dxzbdrvmn/image/upload/v1667543361/Products/Iphone-13_black_dhz5bb.png"
+  },
+  
+];
+
+module.exports = products;
