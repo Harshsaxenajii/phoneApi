a0ee45777cb45179202d8dd9a644c237af01ef8a
harsh saxena <harshsaxenajii@gmail.com>
2023-01-02 17:54:44 +0530

    add backend and database

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const products = require("./products");
+const app = express();
+const mongoose = require("mongoose");
+const register = require("./routes/register")
+const login = require("./routes/login")
+
+app.use(express.json());
+app.use(cors());
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our online shop api...");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+app.use("/api/register",register)
+app.use("/api/login",login)
+
+const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
+app.listen(port, console.log(`Server is running on port ${port}`));
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>console.log("Mongo connected"))
+.catch(()=>console.log("connection error"))
